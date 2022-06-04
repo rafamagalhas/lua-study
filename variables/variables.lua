c14148995696fc1cb6d5b9028bf51a0919faa3ba
rafamagalhas <magalhas.rafa@gmail.com>
2022-06-04 17:43:08 -0300

    feat: lua variables and types

---
+++ variables/variables.lua
@@ -0,0 +1,15 @@
+/* Lua language's variables and yours types*/
+
+bestNumber = 42 -- all numbers are double
+
+immutableString = 'potato' 
+doubleQuotes = "double-quotes are also fine"
+multiLinesString = [[
+  Using double brackets
+  to create a multiline 
+  strings. 
+]]
+undefinedVariable = nil -- Lua has garbage collection
+
+
+
